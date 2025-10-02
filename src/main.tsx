a1e8ae54d6dbecc4d72647a18331404dd5e04929
Ryan P <ryan81.park@gmail.com>
2025-10-02 12:05:43 +1000

    Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
-
+import { Analytics } from "@vercel/analytics/react";
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
@@ -21,6 +21,7 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
+      <Analytics />
       <RouterProvider router={router} />
     </StrictMode>
   );
